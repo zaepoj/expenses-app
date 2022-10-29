1961ae8c5b91646309852b986cb513ba4c97f555
gaepoij <henri.lahti9@gmail.com>
2022-10-29 05:07:08 +0300

    prisma schema updt

--- app/server/db.server.ts
+++ app/server/db.server.ts
@@ -1,5 +1,15 @@
 import { Expense, PrismaClient, User } from "@prisma/client";
-const prisma = new PrismaClient();
+
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
 type StoreUserType = Pick<User, "email" | "name" | "uid">;
 
