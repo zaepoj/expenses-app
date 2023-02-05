d3d8dd1b61ff955ab6b7c17b06ee1e09809aa5ac
Henri Lahti <henri.lahti9@gmail.com>
2023-02-05 18:18:11 +0200

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -36,7 +36,7 @@ const Container = styled.div`
 const OutletContainer = styled.div<{ isLoginOrSignUpPage: boolean }>`
   margin-left: ${(props) => (props.isLoginOrSignUpPage ? "auto" : "250px")};
   margin-right: ${(props) => props.isLoginOrSignUpPage && "auto"};
-  width: ${(props) => !props.isLoginOrSignUpPage && "100%"};
+  width: "100%";
   min-width: 300px;
 
   @media only screen and (max-width: 700px) {
