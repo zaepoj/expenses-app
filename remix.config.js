81d17dcac3108e316b83f678d60ecae7fdde01fc
gaepoij <henri.lahti9@gmail.com>
2023-02-05 16:02:27 +0200

    opt in vanilla extract

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,10 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: ["**/.*"],
+  future: {
+    unstable_vanillaExtract: true,
+  },
+  ignoredRouteFiles: ["**/.*"]
+
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
