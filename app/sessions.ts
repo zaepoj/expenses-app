1285d14a1b019f7c05c88bf6e67d6738bccd2dc3
gaepoij <henri.lahti9@gmail.com>
2023-02-08 21:00:25 +0200

    update secret

--- app/sessions.ts
+++ app/sessions.ts
@@ -4,7 +4,7 @@ export const { getSession, commitSession, destroySession } =
   createCookieSessionStorage({
     cookie: {
       name: "__session",
-      secrets: ["fancy-secret-key"],
+      secrets: [process.env.SessionSecret || "fancy-secret"],
       maxAge: 60 * 60 * 24 * 7, // 1 week
       sameSite: "lax",
       path: "/",
