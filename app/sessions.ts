1a5003e4916d12956ec9fda8ecb7ec917dfb0339
gaepoij <henri.lahti9@gmail.com>
2023-02-09 23:30:49 +0200

    correct case

--- app/sessions.ts
+++ app/sessions.ts
@@ -4,7 +4,7 @@ export const { getSession, commitSession, destroySession } =
   createCookieSessionStorage({
     cookie: {
       name: "__session",
-      secrets: [process.env.SessionSecret || "fancy-secret"],
+      secrets: [process.env.SESSION_SECRET || "fancy-secret"],
       maxAge: 60 * 60 * 24 * 7, // 1 week
       sameSite: "lax",
       path: "/",
