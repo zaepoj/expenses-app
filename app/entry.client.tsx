b01a107f9586c1fa60ea39c14147437e6b174367
gaepoij <henri.lahti9@gmail.com>
2023-07-04 11:18:02 +0300

    try old hydrate

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -8,11 +8,13 @@ import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>
-  );
-});
+require("react-dom").hydrate(<RemixBrowser />, document);
+
+// startTransition(() => {
+//   hydrateRoot(
+//     document,
+//     <StrictMode>
+//       <RemixBrowser />
+//     </StrictMode>
+//   );
+// });
