70c56f935c78463dc1308aa98cf176281cb9c44f
gaepoij <henri.lahti9@gmail.com>
2023-07-07 17:30:22 +0300

    fix db client

--- app/server/db.server.ts
+++ app/server/db.server.ts
@@ -4,13 +4,13 @@ import {
   type User,
 } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = global.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 type StoreUserType = Pick<User, "email" | "name" | "uid">;
 
